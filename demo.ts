d362232a90627a9756a71830d74893999f43b77f
Melissa Spiegel <mrspiegel@me.com>
2025-10-14 19:38:18 -0400

    Implement createCounter function with methods
    Added a createCounter function with increment, decrement, and reset methods.

--- demo.ts
+++ demo.ts
@@ -197,6 +197,50 @@ This passes all LeetCode test cases and runs in O(1) time per operation.
 
 
 
+// 2665. Counter II
+  /**
+ * @param {number} init
+ * @return {{increment: Function, decrement: Function, reset: Function}}
+ */
+/**
+ * @param {number} init
+ * @return {{ increment: Function, decrement: Function, reset: Function }}
+ */
+var createCounter = function(init) {
+  let current = init;
+
+  return {
+    increment: function() {
+      current++;
+      return current;
+    },
+
+    decrement: function() {
+      current--;
+      return current;
+    },
+
+    reset: function() {
+      current = init;
+      return current;
+    }
+  };
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset();     // 5
+ * counter.decrement(); // 4
+ */;
+
+/**
+ * Example usage:
+ * const counter = createCounter(5);
+ * console.log(counter.increment()); // 6
+ * console.log(counter.reset());     // 5
+ * console.log(counter.decrement()); // 4
+ */
 
 
 
